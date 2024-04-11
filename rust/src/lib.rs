77f304e6ffb865ceaa4fe51a194539386179181e
Tae Hagen <tae.hagen@gmail.com>
2024-04-11 16:17:18 -0600

    Use protobuf for hw id sharing for swift compatibility

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -10,6 +10,10 @@ fn runtime() -> &'static Runtime {
     RUNTIME.get_or_init(|| Runtime::new().unwrap())
 }
 
+pub mod bbhwinfo {
+    include!(concat!(env!("OUT_DIR"), "/bbhwinfo.rs"));
+}
+
 mod native;
 pub mod api;
 mod frb_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
